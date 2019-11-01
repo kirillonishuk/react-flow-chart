246eaa545d3a982c863ee6b42481f84e9d30856a
kirill.onishuk <kirill.onishuk@digiforest.io>
2019-11-01 11:02:15 +0300

    Edited space for builder

--- dist/src/components/Canvas/CanvasInner.default.js
+++ dist/src/components/Canvas/CanvasInner.default.js
@@ -5,6 +5,6 @@ var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cook
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var styled_components_1 = require("styled-components");
-exports.CanvasInnerDefault = styled_components_1.default.div(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  position: relative;\n  outline: 1px dashed rgba(0,0,0,0.1);\n  width: 10000px;\n  height: 10000px;\n  cursor: move;\n"], ["\n  position: relative;\n  outline: 1px dashed rgba(0,0,0,0.1);\n  width: 10000px;\n  height: 10000px;\n  cursor: move;\n"])));
+exports.CanvasInnerDefault = styled_components_1.default.div(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  position: relative;\n  outline: 1px dashed rgba(0,0,0,0.1);\n  width: 10000px;\n  height: 20000px;\n  cursor: move;\n"], ["\n  position: relative;\n  outline: 1px dashed rgba(0,0,0,0.1);\n  width: 10000px;\n  height: 20000px;\n  cursor: move;\n"])));
 var templateObject_1;
 //# sourceMappingURL=CanvasInner.default.js.map
--- src/components/Canvas/CanvasInner.default.tsx
+++ src/components/Canvas/CanvasInner.default.tsx
@@ -15,6 +15,6 @@ export const CanvasInnerDefault = styled.div<ICanvasInnerDefaultProps>`
   position: relative;
   outline: 1px dashed rgba(0,0,0,0.1);
   width: 10000px;
-  height: 10000px;
+  height: 20000px;
   cursor: move;
 ` as any
